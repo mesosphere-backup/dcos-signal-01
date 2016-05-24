47389583701354f27541bad3b082fc775b5bfe78
malnick <malnick@gmail..com>
2016-05-24 16:19:52 -0700

    cosmos type

--- signal/cosmos.go
+++ signal/cosmos.go
@@ -2,14 +2,78 @@ package signal
 
 import (
 	"encoding/json"
-	"time"
 
-	log "github.com/Sirupsen/logrus"
+	//	log "github.com/Sirupsen/logrus"
 	"github.com/dcos/dcos-signal/config"
 	"github.com/segmentio/analytics-go"
 )
 
-type HealthReport struct {
-	Units map[string]*Unit
-	Nodes map[string]*Node
+type CosmosReport struct {
+	Packages []struct {
+		AppID string `json:"appId"`
+	} `json:"packages"`
+}
+
+type Cosmos struct {
+	Report  *CosmosReport
+	URL     string
+	Method  string
+	Headers map[string]string
+	Track   *analytics.Track
+}
+
+func (c *Cosmos) SetReport(body []byte) error {
+	var report *CosmosReport
+	if err := json.Unmarshal(body, &report); err != nil {
+		return err
+	}
+	c.Report = report
+	return nil
+}
+
+func (c *Cosmos) GetReport() interface{} {
+	return c.Report
+}
+
+func (c *Cosmos) SetHeaders(headers map[string]string) {
+	c.Headers = headers
+}
+
+func (c *Cosmos) GetHeaders() map[string]string {
+	return c.Headers
+}
+
+func (c *Cosmos) SetURL(url string) {
+	c.URL = url
+}
+
+func (c *Cosmos) GetURL() string {
+	return c.URL
+}
+
+func (c *Cosmos) SetMethod(method string) {
+	c.Method = method
+}
+
+func (c *Cosmos) GetMethod() string {
+	return c.Method
+}
+
+func (c *Cosmos) SetTrack(config config.Config) error {
+	properties := make(map[string]interface{})
+	c.Track = &analytics.Track{
+		Event:       config.SegmentEvent,
+		UserId:      config.CustomerKey,
+		AnonymousId: config.ClusterID,
+		Properties:  properties,
+	}
+	return nil
+}
+
+func (c *Cosmos) GetTrack() *analytics.Track {
+	return c.Track
+}
+
+func (c *Cosmos) SendTrack(config config.Config) error {
+	return nil
 }
