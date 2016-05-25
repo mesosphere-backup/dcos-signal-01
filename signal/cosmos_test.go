16557a25e8b1000a7157d5618698b8a50859e7f1
malnick <malnick@gmail..com>
2016-05-25 13:35:45 -0700

    Adding cosmos test on properties

--- signal/cosmos_test.go
+++ signal/cosmos_test.go
@@ -34,6 +34,44 @@ func TestCosmosTrack(t *testing.T) {
 		t.Error("Expected no errors setting up track, got", setupErr)
 	}
 
-	actualTrack := testCosmos.GetTrack()
+	actualSegmentTrack := testCosmos.GetTrack()
+	if actualSegmentTrack.Event != "package_list" {
+		t.Error("Expected actualSegmentTrack.Event to be 'package_list', got ", actualSegmentTrack.Event)
+	}
 
+	if actualSegmentTrack.UserId != "12345" {
+		t.Error("Expected actual segment track user ID to be 12345, got ", actualSegmentTrack.UserId)
+	}
+
+	if actualSegmentTrack.AnonymousId != "anon" {
+		t.Error("Expected anon ID to be 'anon', got ", actualSegmentTrack.AnonymousId)
+	}
+
+	if actualSegmentTrack.Properties["clusterId"] != "anon" {
+		t.Error("Expected clusterId to be anon, got ", actualSegmentTrack.Properties["clusterId"])
+	}
+
+	if actualSegmentTrack.Properties["source"] != "cluster" {
+		t.Error("Expected source to be cluster, got ", actualSegmentTrack.Properties["source"])
+	}
+
+	if actualSegmentTrack.Properties["customerKey"] != "12345" {
+		t.Error("Expected customerKey to be 12345, got ", actualSegmentTrack.Properties["customerKey"])
+	}
+
+	if actualSegmentTrack.Properties["provider"] != "test_provider" {
+		t.Error("Expected provder 'test_provider', got ", actualSegmentTrack.Properties["provider"])
+	}
+
+	if actualSegmentTrack.Properties["variant"] != "test_variant" {
+		t.Error("Expected variant 'test_variant', got ", actualSegmentTrack.Properties["variant"])
+	}
+
+	if actualSegmentTrack.Properties["environmentVersion"] != "test_version" {
+		t.Error("Expected environmenetVersion 'test_varsion', got ", actualSegmentTrack.Properties["environmentVersion"])
+	}
+
+	if len(actualSegmentTrack.Properties["package_list"].([]CosmosPackages)) != 1 {
+		t.Error("Expected 1 package in list, got", len(actualSegmentTrack.Properties["package_list"].([]CosmosPackages)))
+	}
 }
