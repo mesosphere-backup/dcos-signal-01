0528b0bb89f4e74b34b10fc53c9698f294d180d1
Drew Flower <dflower@mesosphere.com>
2019-04-17 19:21:57 -0600

    Add integration testing to segment
    New integration test that calls segment.com, in order to verify
    events are being sent and captured as expected.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/Sirupsen/logrus v0.8.7
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/google/uuid v1.1.1
 	github.com/gorilla/context v0.0.0-20150820051245-1c83b3eabd45 // indirect
 	github.com/gorilla/mux v0.0.0-20150908165839-49c024275504
 	github.com/jehiah/go-strftime v0.0.0-20151206194810-2efbe75097a5 // indirect
---
+++ signal/signal_integration_test.go
@@ -0,0 +1,35 @@
+// +build integration
+
+package signal
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/uuid"
+	analytics "github.com/segmentio/analytics-go"
+)
+
+// TestSignalIntegration requires the SEGMENT_WRITE_KEY environment
+// variable.
+func TestSignalIntegration(t *testing.T) {
+
+	m := Mesos{
+		Track: &analytics.Track{
+			Event:  "mesos_integration_test",
+			UserId: uuid.New().String(),
+			Properties: map[string]interface{}{
+				"license_id": "local_integration_test",
+			},
+		},
+	}
+
+	client := analytics.New(os.Getenv("SEGMENT_WRITE_KEY"))
+	client.Size = 1
+	defer client.Close()
+
+	err := client.Track(m.Track)
+	if err != nil {
+		t.Error("Failed to send track. Got error: ", err)
+	}
+}
