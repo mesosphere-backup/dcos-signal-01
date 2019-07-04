mesosphere-backup/dcos-signal-01

commit 2615bc7f7d524858be0c5ed603fb24e7d35d9292

Get linting working

signal/signal.go
@@ -40,7 +40,7 @@ func runner(done chan Reporter, reporters chan Reporter, c config.Config, w int)
 
 func executeTester(data map[string]*analytics.Track, c config.Config) error {
 	jsonStr, err := json.MarshalIndent(data, "", "    ")
-	fmt.Printf(string(jsonStr))
+	fmt.Print(string(jsonStr))
 	return err
 }
 
@@ -58,31 +58,31 @@ func executeRunner(c config.Config) error {
 	workers := len(reporters)
 	for w := 1; w <= workers; w++ {
 		log.Debugf("Deploying Worker %d", w)
+		// `runner` probably shouldn't be returning an error but should send that to the reporters
+		// channel. However that's a large change so we ignore this check here
+		// nolint: errcheck
 		go runner(done, reporters, c, w)
 	}
 
 	tester := make(map[string]*analytics.Track)
 
 	processed := 1
-	for processed <= workers {
-		select {
-		case r := <-done:
+	for r := range done {
+		for _, err := range r.getError() {
+			log.Errorf("%s: %s", r.getName(), err)
+		}
+		if c.FlagTest {
+			log.Debugf("Adding test data for %s: %+v", r.getName(), r.getTrack())
+			tester[r.getName()] = r.getTrack()
+		} else if len(r.getError()) > 0 {
 			for _, err := range r.getError() {
 				log.Errorf("%s: %s", r.getName(), err)
 			}
-			if c.FlagTest {
-				log.Debugf("Adding test data for %s: %+v", r.getName(), r.getTrack())
-				tester[r.getName()] = r.getTrack()
-			} else if len(r.getError()) > 0 {
-				for _, err := range r.getError() {
-					log.Errorf("%s: %s", r.getName(), err)
-				}
-			} else {
-				r.sendTrack(c)
-			}
-			log.Warnf("processed %d, workers %d", processed, workers)
-			processed++
+		} else {
+			_ = r.sendTrack(c)
 		}
+		log.Warnf("processed %d, workers %d", processed, workers)
+		processed++
 	}
 
 	if c.FlagTest {

scripts/mocklicensing/mock_license_server.go
@@ -45,7 +45,7 @@ func (l Licenses) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	_, _ = w.Write(js)
 }
 
 const SOCK = "/tmp/dcos-licensing.socket"
@@ -93,5 +93,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	server.Serve(unixListener)
+	err = server.Serve(unixListener)
+	if err != nil {
+		panic(err)
+	}
 }

scripts/test.sh
@@ -37,7 +37,7 @@ function _unittest_with_coverage {
 
 # Main.
 function main {
-    # _lint
+    _lint
     _unittest_with_coverage
 }
 

commit a3e33a833a83fcc6cab90f7b2c771de6822bb869

Fixed deadlock when processing workers
2615bc7f7d524858be0c5ed603fb24e7d35d9292 introduced a deadlock since
the `done` channel is never closed and the loop just sits there
waiting for more data from the channel.

signal/signal.go
@@ -67,7 +67,8 @@ func executeRunner(c config.Config) error {
 	tester := make(map[string]*analytics.Track)
 
 	processed := 1
-	for r := range done {
+	for processed <= workers {
+		r := <-done
 		for _, err := range r.getError() {
 			log.Errorf("%s: %s", r.getName(), err)
 		}